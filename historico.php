36e70182634824ef4df882ebdf9328e939ca2f8a
Márcio Lopes <marcioadrianolopesjunior@gmail.com>
2024-09-12 00:55:05 -0300

    Fix history UI issue and new logout function.

--- historico.php
+++ historico.php
@@ -25,6 +25,11 @@ $actionHistory = loadActionHistory();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        .list-group-item {
+            word-wrap: break-word; /* Quebra de linha para evitar ultrapassar a tela */
+        }
+    </style>
 </head>
 <body>
     <!-- Navbar -->
